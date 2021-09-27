9b47122501400017061baf85686a734d50085fd6
Lauribee <laura_stroe_2001@yahoo.com>
2021-09-27 11:49:10 +0200

    Hjælp til planeterne (rum)

--- src/company/Adventure.java
+++ src/company/Adventure.java
@@ -13,34 +13,34 @@ public class Adventure {
         //TODO: Giv rum og hjælp bedre beskrivelser
         Room theEarth1 = new Room("the Earth (1)", """
                 Earth—our home planet—is the only place we know of so far that’s inhabited by living things. 
-                It's also the only planet in our solar system with liquid water on the surface.""","Hjælp til rum1");
+                It's also the only planet in our solar system with liquid water on the surface.""","You are standing on Earth! There are two teleporters, one is at west, and the other at east. Try, and see where you can go!");
         Room saturn2 = new Room("Saturn (2)", """
                 Adorned with a dazzling, complex system of icy rings, Saturn is unique in our solar system. 
-                The other giant planets have rings, but none are as spectacular as Saturn's.""","Hjælp til rum2");
+                The other giant planets have rings, but none are as spectacular as Saturn's.""","You can just pass by this one, there's nothing to see here! Winter on Earth suddenly doesn't seem so bad anymore!");
         Room neptune3 = new Room("Neptune (3)", """
                 Neptune —the eighth and most distant major planet orbiting our Sun—
                 is dark, cold and whipped by supersonic winds. 
-                It was the first planet located through mathematical calculations, rather than by telescope.""","Hjælp til rum3");
+                It was the first planet located through mathematical calculations, rather than by telescope.""","Hurry up and get out of here, before you get blown away by these powerful winds! Quick! Go south!");
         Room jupiter4 = new Room("Jupiter (4)", """
                 Jupiter is more than twice as massive than the other planets of our solar system combined. 
-                The giant planet's Great Red spot is a centuries-old storm bigger than Earth.""","Hjælp til rum4");
+                The giant planet's Great Red spot is a centuries-old storm bigger than Earth.""","Oh God! The storm! I can't see a thing! Hurry! Get out of here!");
         Room pluto5 = new Room("Pluto (5)", """
                 Pluto is a complex world of ice mountains and frozen plains. 
                 Once considered the ninth planet, Pluto is the largest member of the Kuiper Belt 
-                and the best known of a new class of worlds called dwarf planets.""","Hjælp til rum5");
+                and the best known of a new class of worlds called dwarf planets.""","We are finally here! Oh it's so peaceful here. So quiet, let's stay here for ever.");
         Room mercury6 = new Room("Mercury (6)", """
                 Mercury—the smallest planet in our solar system and closest to the Sun—
                 is only slightly larger than Earth's Moon. 
-                Mercury is the fastest planet, zipping around the Sun every 88 Earth days.""","Hjælp til rum6");
+                Mercury is the fastest planet, zipping around the Sun every 88 Earth days.""","From freezing cold to flaming HOT! This is killing me! Hurry! Find your way to a safer place!");
         Room venus7 = new Room("Venus (7)", """
                 Venus spins slowly in the opposite direction from most planets. 
                 A thick atmosphere traps heat in a runaway greenhouse effect, making it the hottest planet in our solar system.""","Hjælp til rum7");
         Room mars8 = new Room("Mars (8)", """
                 Mars is a dusty, cold, desert world with a very thin atmosphere. 
-                There is strong evidence Mars was —billions of years ago— wetter and warmer, with a thicker atmosphere.""","Hjælp til rum8");
+                There is strong evidence Mars was —billions of years ago— wetter and warmer, with a thicker atmosphere.""","I can sense it! WE ARE SO CLOSE!!! Don't lose it now! \nOH NO! Some aliens are blocking the portal, quick! Do something! ");
         Room uranus9 = new Room(" Uranus (9)", """
                 Uranus—seventh planet from the Sun—rotates at a nearly 90-degree angle from the plane of its orbit. 
-                This unique tilt makes Uranus appear to spin on its side.""","Hjælp til rum9");
+                This unique tilt makes Uranus appear to spin on its side.""","BRRR! Not exactly Utopia, is it?");
 
         //lav forbindelse mellem rummene
         theEarth1.createDoorEast(saturn2);
