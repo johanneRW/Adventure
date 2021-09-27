c2392dcc3e3027e5ae182d6204f4923ba59d63d3
johanne <johanne_riis@hotmail.com>
2021-09-27 09:52:34 +0200

    sout

--- src/company/Adventure.java
+++ src/company/Adventure.java
@@ -7,7 +7,7 @@ public class Adventure {
     public static void main(String[] args) {
         boolean gameRunning = true;
         Scanner input = new Scanner(System.in);
-        //TODO: beslut om spillet skal tale dansk eller engelsk med brugeren
+        //TODO: Rumnavne og beskrivelser skal være på engelsk, og sjovere end room1
 
         //Lav de ønskede rum
         //TODO: Opret alle rum.
@@ -39,8 +39,10 @@ public class Adventure {
                 You can also type the first letter of the direction you want to move.
                 If you want to look around, just type "look" or 'l', type "help" or 'h' for help.
                 If you want to quit the game, type "exit".
+                """);
+                System.out.println(currentRoom.getROOM_DESCRIPTION());
+        System.out.println("What do you want to do first?");
 
-                What do you want to do first?""");
         while (gameRunning) {
             String command = input.nextLine();
             command = command.toLowerCase();
@@ -48,39 +50,44 @@ public class Adventure {
             if (command.equals("go north") || command.equals("n") || command.equals("north")) {
                 if (currentRoom.getDoorNorth() != null) {
                     currentRoom = currentRoom.getDoorNorth();
-                    System.out.println("Going north");
+                    System.out.println("Going north...");
+                    System.out.println(currentRoom.getROOM_DESCRIPTION());
                 } else {
                     System.out.println("You can't go that way");
                 }
             } else if (command.equals("go west") || command.equals("w") || command.equals("west")) {
                 if (currentRoom.getDoorWest() != null) {
                     currentRoom = currentRoom.getDoorWest();
-                    System.out.println("Going west");
+                    System.out.println("Going west...");
+                    System.out.println(currentRoom.getROOM_DESCRIPTION());
                 } else {
                     System.out.println("You can't go that way");
                 }
             } else if (command.equals("go east") || command.equals("e") || command.equals("east")) {
                 if (currentRoom.getDoorEast() != null) {
                     currentRoom = currentRoom.getDoorEast();
-                    System.out.println("Going east");
+                    System.out.println("Going east...");
+                    System.out.println(currentRoom.getROOM_DESCRIPTION());
                 } else {
                     System.out.println("You can't go that way");
                 }
             } else if (command.equals("go south") || command.equals("s") || command.equals("south")) {
                 if (currentRoom.getDoorSouth() != null) {
                     currentRoom = currentRoom.getDoorSouth();
-                    System.out.println("Going south");
+                    System.out.println("Going south...");
+                    System.out.println(currentRoom.getROOM_DESCRIPTION());
                 } else {
                     System.out.println("You can't go that way");
                 }
             } else if (command.equals("look") || command.equals("l")) {
-                System.out.println("Looking around");
+                System.out.println("Looking around...");
                 System.out.println(currentRoom.getROOM_DESCRIPTION());
             } else if (command.equals("help") || command.equals("h")) {
                 System.out.println("Want help?");
-                //TODO: Måske skal der indsættes endnu en if-sætning, hvor brugeren kan sige ja eller nej?
+                //TODO: Måske skal der indsættes endnu en if-sætning, hvor brugeren kan sige ja eller nej? ellers skal "help" kalde en hjælpe tekst.
             } else if (command.equals("exit")) {
                 System.out.println("Goodbye!");
+                System.out.println("see you another time");
                 gameRunning = false;
             } else {
                 //hvis spilleren taster en ugyldig kommando, beder spillet om en ny, dette er for at Adventure ikke crasher ved ugyldigt indput.
