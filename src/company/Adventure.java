3d7ea709d202793dc1781fad8b75fb56177ea3d4
johanne <johanne_riis@hotmail.com>
2021-09-27 18:57:12 +0200

    planeterne er flyttet til Map, og teksterne er rettet for stavefejl. check om currentRoom er sidste rum er nu flyttet til checkIfFinal.

--- src/company/Adventure.java
+++ src/company/Adventure.java
@@ -4,59 +4,12 @@ import java.util.Scanner;
 
 public class Adventure {
 
+    private static boolean gameRunning = true;
+    private static Map spaceMap =new Map();
+
     public static void main(String[] args) {
-        boolean gameRunning = true;
+
         Scanner input = new Scanner(System.in);
-
-        //Lav de ønskede rum, beskrivelser og hjælp-tekst.
-        Room theEarth1 = new Room("the Earth (1)", """
-                Earth — our home planet — is the only place we know of so far that’s inhabited by living things.
-                It's also the only planet in our solar system with liquid water on the surface.""", "You are standing on Earth! There are two teleporters, one is at west, and the other at east. Try, and see where you can go!");
-        Room saturn2 = new Room("Saturn (2)", """
-                Adorned with a dazzling, complex system of icy rings, Saturn is unique in our solar system.
-                The other giant planets have rings, but none are as spectacular as Saturn's.""", "You can just pass by this one, there's nothing to see here, except for these spectacular rings!! Winter on Earth suddenly doesn't seem so bad anymore though!");
-        Room neptune3 = new Room("Neptune (3)", """
-                Neptune — the eighth and most distant major planet orbiting our Sun—
-                is dark, cold and whipped by supersonic winds.
-                It was the first planet located through mathematical calculations, rather than by telescope.""", "Hurry up and get out of here, before you get blown away by these powerful winds! Quick! Go south!");
-        Room jupiter4 = new Room("Jupiter (4)", """
-                Jupiter is more than twice as massive than the other planets of our solar system combined.
-                The giant planet's Great Red spot is a centuries-old storm bigger than Earth.""", "Oh God! The storm! I can't see a thing! Hurry! Get out of here!");
-        Room pluto5 = new Room("Pluto (5)", """
-                YOU FOUND IT!! What a marvellous feat! 
-                
-                Pluto is a complex world of ice mountains and frozen plains.
-                Once considered the ninth planet, Pluto is the largest member of the Kuiper Belt.
-                and the best known of a new class of worlds called dwarf planets, now Pluto can be returned to its rightful place among the real planets.""", "We are finally here! Oh it's so peaceful here. So quiet, let's stay here for ever.");
-        Room mercury6 = new Room("Mercury (6)", """
-                Mercury — the smallest planet in our solar system and closest to the Sun —
-                is only slightly larger than Earth's Moon.
-                Mercury is the fastest planet, zipping around the Sun every 88 Earth days.""", "From freezing cold to flaming HOT! This is killing me! Hurry! Find your way to a safer place!");
-        Room venus7 = new Room("Venus (7)", """
-                Venus spins slowly in the opposite direction from most planets.
-                A thick atmosphere traps heat in a runaway greenhouse effect, making it the hottest planet in our solar system.""", "If you want to continue getting my help, you better get your butt out of here, cause my motherboard is quickly melting :(!");
-        Room mars8 = new Room("Mars (8)", """
-                Mars is a dusty, cold, desert world with a very thin atmosphere.
-                There is strong evidence Mars was — billions of years ago — wetter and warmer, with a thicker atmosphere.""", "I can sense it! WE ARE SO CLOSE!!! Don't lose it now! \nOH NO! Some aliens are blocking the portal, quick! Do something! ");
-        Room uranus9 = new Room("Uranus (9)", """
-                Uranus — seventh planet from the Sun — rotates at a nearly 90-degree angle from the plane of its orbit.
-                This unique tilt makes Uranus appear to spin on its side.""", "BRRR! Not exactly Utopia, is it?");
-
-        //lav forbindelse mellem rummene
-        theEarth1.createDoorEast(saturn2);
-        theEarth1.createDoorSouth(jupiter4);
-        saturn2.createDoorEast(neptune3);
-        neptune3.createDoorSouth(mercury6);
-        jupiter4.createDoorSouth(venus7);
-        pluto5.createDoorSouth(mars8);
-        mercury6.createDoorSouth(uranus9);
-        venus7.createDoorEast(mars8);
-        mars8.createDoorEast(uranus9);
-
-        //I følge instruktionen skal bruger starte i rum 1, derfor er dette startværdien.
-
-        Room currentRoom = theEarth1;
-        Room finalRoom = pluto5;
 
 
         //kommandoer: "go north", "go east", "go south","go west", "n", "e", "s", "w",
@@ -65,20 +18,20 @@ public class Adventure {
         //look - for at få gentaget beskrivelsen af det rum man er i
 
         System.out.println("""
-                Welcome to 'the rediscovering of Pluto'-Adventure game.
+                Welcome to The Rediscovering of Pluto.
                                 
-                I the year 2006 we lost our smallest planet, it was demoted, and categorised as a dwarf planet.
-                But we belive that Pluto belongs among the real planets, so your mission is:
+                In the year 2006 we lost our smallest planet. It was demoted, and categorised as a dwarf planet.
+                But we believe that Pluto belongs to the real planets, so your mission is:
                 To rediscover our small friend - Pluto.
-                On you quest you will find some teleportes that will send you to other plannets.
+                On you quest you will find a number of teleporters that will send you to other planets.
                                 
                 How to play:
                 You can move around in the game by typing "go" followed by "north", "south", "east", "west".
                 You can also type the first letter of the direction you want to move.
-                If you want to look around, just type "look" or 'l', type "help" or 'h' for help.
+                If you want to look around, just type "look" or 'l'. Type "help" or 'h' for help.
                 If you want to quit the game, type "exit".
                                 
-                At this moment you are placed on Earth.
+                At this moment you are on Earth. Take a look around.
                 What do you want to do first?""");
         while (gameRunning) {
             String command = input.nextLine();
@@ -95,63 +48,51 @@ public class Adventure {
                     System.out.println("Alright! Let's keep going then :)");
                 }
             } else if (command.equals("go north") || command.equals("n") || command.equals("north")) {
-                if (currentRoom.getDoorNorth() != null) {
-                    currentRoom = currentRoom.getDoorNorth();
-                    System.out.println("Teleporting north...");
-                    System.out.println(currentRoom.getROOM_DESCRIPTION());
-                    if (currentRoom.equals(finalRoom)) {
-                        gameRunning = false;
-                        System.out.println("CONGRATULATION!!\n\nThe End");
-                    }
+                if (spaceMap.currentRoom.getDoorNorth() != null) {
+                    spaceMap.currentRoom = spaceMap.currentRoom.getDoorNorth();
+                    System.out.println("Teleporting north...\n");
+                    System.out.println(spaceMap.currentRoom.getROOM_DESCRIPTION());
+                   checkIfFinal();
                 } else {
-                    System.out.println("You can't go that way, ther isn't a teleporter.");
+                    System.out.println("You can't go that way - there isn't a teleporter.");
                 }
             } else if (command.equals("go west") || command.equals("w") || command.equals("west")) {
-                if (currentRoom.getDoorWest() != null) {
-                    currentRoom = currentRoom.getDoorWest();
-                    System.out.println("Teleporting west...");
-                    System.out.println(currentRoom.getROOM_DESCRIPTION());
-                    if (currentRoom.equals(finalRoom)) {
-                        gameRunning = false;
-                        System.out.println("CONGRATULATION!!\n\nThe End");
-                    }
+                if (spaceMap.currentRoom.getDoorWest() != null) {
+                    spaceMap.currentRoom = spaceMap.currentRoom.getDoorWest();
+                    System.out.println("Teleporting west...\n");
+                    System.out.println(spaceMap.currentRoom.getROOM_DESCRIPTION());
+                    checkIfFinal();
                 } else {
-                    System.out.println("You can't go that way, ther isn't a teleporter.");
+                    System.out.println("You can't go that way - there isn't a teleporter.");
                 }
             } else if (command.equals("go east") || command.equals("e") || command.equals("east")) {
-                if (currentRoom.getDoorEast() != null) {
-                    currentRoom = currentRoom.getDoorEast();
-                    System.out.println("Teleporting east...");
-                    System.out.println(currentRoom.getROOM_DESCRIPTION());
-                    if (currentRoom.equals(finalRoom)) {
-                        gameRunning = false;
-                        System.out.println("CONGRATULATION!!\n\nThe End");
-                    }
+                if (spaceMap.currentRoom.getDoorEast() != null) {
+                    spaceMap.currentRoom = spaceMap.currentRoom.getDoorEast();
+                    System.out.println("Teleporting east...\n");
+                    System.out.println(spaceMap.currentRoom.getROOM_DESCRIPTION());
+                    checkIfFinal();
                 } else {
-                    System.out.println("You can't go that way, ther isn't a teleporter.");
+                    System.out.println("You can't go that way - there isn't a teleporter.");
                 }
             } else if (command.equals("go south") || command.equals("s") || command.equals("south")) {
-                if (currentRoom.getDoorSouth() != null) {
-                    currentRoom = currentRoom.getDoorSouth();
-                    System.out.println("Teleporting south...");
-                    System.out.println(currentRoom.getROOM_DESCRIPTION());
-                    if (currentRoom.equals(finalRoom)) {
-                        gameRunning = false;
-                        System.out.println("CONGRATULATION!!\n\nThe End");
-                    }
+                if (spaceMap.currentRoom.getDoorSouth() != null) {
+                    spaceMap.currentRoom = spaceMap.currentRoom.getDoorSouth();
+                    System.out.println("Teleporting south...\n");
+                    System.out.println(spaceMap.currentRoom.getROOM_DESCRIPTION());
+                    checkIfFinal();
                 } else {
-                    System.out.println("You can't go that way, ther isn't a teleporter.");
+                    System.out.println("You can't go that way - there isn't a teleporter.");
                 }
             } else if (command.equals("look") || command.equals("l")) {
                 System.out.println("Looking around...");
-                System.out.println(currentRoom.getROOM_DESCRIPTION());
+                System.out.println(spaceMap.currentRoom.getROOM_DESCRIPTION());
             } else if (command.equals("help") || command.equals("h")) {
                 System.out.println("Want help? Type \"yes\" or \"no\"");
                 String answer = input.nextLine();
                 answer = answer.toLowerCase();
                 if (answer.equals("yes")) {
-                    System.out.println(" Okay, I'll call headquarter, they will help you - maybe.   .");
-                    System.out.println(currentRoom.getROOM_HELP());
+                    System.out.println("Okay, I'll call back to headquarters, they will help you - maybe...");
+                    System.out.println(spaceMap.currentRoom.getROOM_HELP());
                 } else if (answer.equals("no")) {
                     System.out.println("Okay, suit yourself! :)");
                 }
@@ -160,6 +101,13 @@ public class Adventure {
                 System.out.println("I don't know how to \"" + command + "\", try typing something else");
             }
 
+        }
+    }
+
+    private static void checkIfFinal() {
+        if (spaceMap.currentRoom.equals(spaceMap.getFinalRoom())) {
+            gameRunning = false;
+            System.out.println("\nYou have reached the end of the game. CONGRATULATIONS!!!\nThe End\nNow, go out and look at the sky.");
         }
     }
 }
---
+++ src/company/Map.java
@@ -0,0 +1,75 @@
+package company;
+
+public class Map {
+    public Room currentRoom;
+    private Room finalRoom;
+
+    public Map() {
+        //Lav de ønskede rum, beskrivelser og hjælp-tekst.
+
+        //Planeternes beskrivelser er hentet på https://solarsystem.nasa.gov/planets/overview/
+
+        Room theEarth1 = new Room("the Earth (1)", """
+                Earth — our home planet — is the only place we know of so far that’s inhabited by living things.
+                It's also the only planet in our solar system with liquid water on the surface.""",
+                "You are standing on Earth! There are two teleporters, one is to the west, and the other to the east. Try one of them and see where you land!");
+        Room saturn2 = new Room("Saturn (2)", """
+                Adorned with a dazzling, complex system of icy rings, Saturn is unique in our solar system.
+                The other giant planets have rings, but none are as spectacular as Saturn's.""",
+                "You can just pass by this one, there's nothing to see here, except for these spectacular rings!! Winter on Earth suddenly doesn't seem so bad anymore though!");
+        Room neptune3 = new Room("Neptune (3)", """
+                Neptune — the eighth and most distant major planet orbiting our Sun —
+                is dark, cold and whipped by supersonic winds.
+                It was the first planet located through mathematical calculations, rather than by telescope.""",
+                "Hurry up and get out of here, before you get blown away by these powerful winds! Quick! Go south!");
+        Room jupiter4 = new Room("Jupiter (4)", """
+                Jupiter is more than twice as massive than the other planets of our solar system combined.
+                The giant planet's Great Red spot is a centuries-old storm bigger than Earth.""",
+                "Oh God! The storm! I can't see a thing! Hurry! Get out of here!");
+        Room pluto5 = new Room("Pluto (5)", """
+                YOU FOUND IT!! What a marvellous feat!
+                                
+                Pluto is a complex world of ice mountains and frozen plains.
+                Still considered the ninth planet by some, Pluto is the largest member of the Kuiper Belt.
+                It is the best known of a new class of worlds called dwarf planets. But now Pluto can be returned to its rightful place among the other planets.""",
+                "We are finally here! Oh, it's so peaceful here. So quiet, let's stay here forever.");
+        Room mercury6 = new Room("Mercury (6)", """
+                Mercury — the smallest planet in our solar system and closest to the Sun —
+                is only slightly larger than Earth's Moon.
+                Mercury is the fastest planet, zipping around the Sun every 88 Earth days.""",
+                "From freezing cold to flaming HOT! This is killing me! Hurry! Find your way to a safer place!");
+        Room venus7 = new Room("Venus (7)", """
+                Venus spins slowly in the opposite direction from most planets.
+                A thick atmosphere traps heat in a runaway greenhouse effect, making it the hottest planet in our solar system.""",
+                "If you want to continue getting my help, you better get your butt out of here, 'cause my motherboard is quickly melting :(!");
+        Room mars8 = new Room("Mars (8)", """
+                Mars is a dusty, cold, desert world with a very thin atmosphere.
+                There is strong evidence Mars was — billions of years ago — wetter and warmer, with a thicker atmosphere.""",
+                "I can sense it! WE ARE SO CLOSE!!! Don't lose it now! \nOH NO! Some aliens are blocking the portal, quick! Do something! ");
+        Room uranus9 = new Room("Uranus (9)", """
+                Uranus — seventh planet from the Sun — rotates at a nearly 90-degree angle from the plane of its orbit.
+                This unique tilt makes Uranus appear to spin on its side.""",
+                "BRRR! Not exactly Utopia, is it?");
+
+        //lav forbindelse mellem rummene
+        theEarth1.createDoorEast(saturn2);
+        theEarth1.createDoorSouth(jupiter4);
+        saturn2.createDoorEast(neptune3);
+        neptune3.createDoorSouth(mercury6);
+        jupiter4.createDoorSouth(venus7);
+        pluto5.createDoorSouth(mars8);
+        mercury6.createDoorSouth(uranus9);
+        venus7.createDoorEast(mars8);
+        mars8.createDoorEast(uranus9);
+
+        //Ifølge instruktionen skal bruger starte i rum 1, derfor er dette startværdien.
+        this.currentRoom = theEarth1;
+
+        //Det hemmelige rum, hvor spillet slutter
+        this.finalRoom = pluto5;
+    }
+
+    public Room getFinalRoom() {
+        return finalRoom;
+    }
+}
